f908db1cd03bce297fc0482735eb85c6088db32a
milida <i.milewska@gmail.com>
2020-06-02 17:36:18 +0200

    dodanie przykładowego programu z kolejką FIFO

--- main.c
+++ main.c
@@ -4,11 +4,48 @@
 #include <pthread.h>
 #include <string.h>
 
-void *printString(void *ptr); //KOD Z MUTEXAMI Z ZAJĘĆ
+void *printString(void *ptr);
 
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 
 int main() {
+    //rozmowa z kolejką FIFO
+    int fd;
+
+    // FIFO file path
+    char * myfifo = "/tmp/myfifo";
+
+    // Creating the named file(FIFO)
+    // mkfifo(<pathname>, <permission>)
+    mkfifo(myfifo, 0666);
+
+    char arr1[80], arr2[80];
+    while (1)
+    {
+        // Open FIFO for write only
+        fd = open(myfifo, O_WRONLY);
+
+        // Take an input arr2ing from user.
+        // 80 is maximum length
+        fgets(arr2, 80, stdin);
+
+        // Write the input arr2ing on FIFO
+        // and close it
+        write(fd, arr2, strlen(arr2)+1);
+        close(fd);
+
+        // Open FIFO for Read only
+        fd = open(myfifo, O_RDONLY);
+
+        // Read from FIFO
+        read(fd, arr1, sizeof(arr1));
+
+        // Print the read message
+        printf("User2: %s\n", arr1);
+        close(fd);
+    }
+    return 0;
+    /* // drukarka z mutexem
     pthread_t thread1, thread2;
     int iret1;
 
@@ -47,6 +84,6 @@ void *printString( void *ptr ) {
     // drukowanie wiadomosci znak po znaku
     for(i=0; i<len; i++){
         screenPrinter(message[i]);
-    }
+    }*/
 
 }
